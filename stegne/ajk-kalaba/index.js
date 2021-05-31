ea7ba6308d39902d6695ffbb73c61b9722fbff0e
Jakob Udovic <ju8168@student.uni-lj.si>
2021-06-01 01:06:13 +0200

    add: ajk kalaba

---
+++ stegne/ajk-kalaba/index.js
@@ -0,0 +1,9 @@
+setTimeout(()=>{
+    var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date+' '+time;
+  var time=document.getElementById("time")
+  time.innerText=dateTime
+  },1000)
+  
