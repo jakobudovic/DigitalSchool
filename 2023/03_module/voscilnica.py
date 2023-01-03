3e63a15e5224cccd9ffeabd7fd19fa77713a26b7
jakobudovic <ju8168@student.uni-lj.si>
2023-01-03 18:16:09 +0100

    Add voscilnica

---
+++ 2023/03_module/voscilnica.py
@@ -0,0 +1,20 @@
+zamik = "     "
+
+print(f"&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+print(zamik * 2 + "MERRY CHRISTMAS")
+print(zamik + "AND A HAPPY NEW YEAR")
+print(f"&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+
+
+print()
+for i in range(0, 10):
+    print(" " * i + "jakob")
+
+
+# from colorama import Back, Fore, Style
+
+# print(Fore.RED + "some red text")
+# print(Back.GREEN + "and with a green background")
+# print(Style.DIM + "and in dim text")
+# print(Style.RESET_ALL)
+# print("back to normal now")
