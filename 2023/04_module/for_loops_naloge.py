9c3b8185c2f768e7574425b524a2d8f6cbe7bec5
jakobudovic <ju8168@student.uni-lj.si>
2023-02-09 21:31:57 +0100

    Add for loops exercises

---
+++ 2023/04_module/for_loops_naloge.py
@@ -0,0 +1,132 @@
+"""
+1. 
+Print First 10 natural numbers using for loop
+1.b 
+Print First 10 natural numbers using for loop in the same row, separate with commas.
+"""
+
+# for n in range(1, 11):
+#     print(n)
+
+
+"""
+2. 
+Print the following pattern
+
+Input: 3
+
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+"""
+for vrstica in range(1, 4):
+    print(f"vrstica {vrstica}")
+    for j in range(1, 4):
+        print(f"{j}", end=" ")
+    print(f"")
+
+
+"""
+3.
+Calculate the sum of all numbers from 1 to a given number
+
+Example
+Enter number: 10
+Sum is:  55
+(1 + 2 + 3 + ... + 10)
+"""
+
+"""
+4.
+Write a program to print multiplication table (10 numbers) of a given number.
+
+Example
+Enter number: 2
+2
+4
+6
+8
+...
+"""
+
+"""
+5. 
+Display numbers from a list using loop
+"""
+
+"""
+6.
+Display numbers from a list using loop, but skip every other number. (preskoci vsako 2. stevilko)
+Use this list:
+numbers = [12, 75, 150, 180, 145, 525, 50]
+Out: 
+12
+150
+145
+50
+"""
+
+"""
+7.
+Display numbers from a list using loop, but
+    - The number must be divisible by five
+    - If the number is greater than 150, then skip it and move to the next number
+    - If the number is greater than 500, then stop the loop
+Use this list:
+numbers = [12, 75, 150, 180, 145, 525, 50]
+"""
+
+"""
+8.
+Count the total number of digits in a number, using for loop
+For example, the number is 75869, so the output should be 5.
+"""
+
+"""
+9.*
+Write a program to use for loop to print the following reverse number pattern.
+
+Example
+Enter number: 5
+5 4 3 2 1 
+4 3 2 1 
+3 2 1 
+2 1 
+1
+"""
+
+"""
+10.
+Display numbers from -10 to -1 using for loop
+"""
+
+"""
+11.*
+Display Fibonacci series up to 10 terms, using for loop.
+
+Kaj je fibonacci series?
+The Fibonacci Sequence is a series of numbers. The next number is found by adding up the two numbers before it. 
+The first two numbers are 0 and 1.
+"""
+
+"""
+12.*
+Print the following pattern.
+
+Enter number: 5
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+"""
+
+"""
+.
+"""
