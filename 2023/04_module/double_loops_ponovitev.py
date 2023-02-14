4fae3044defc017388e2d86454f6620974c55d15
jakobudovic <ju8168@student.uni-lj.si>
2023-02-14 19:40:12 +0100

    Add extra exercises for double loops

---
+++ 2023/04_module/double_loops_ponovitev.py
@@ -0,0 +1,81 @@
+"""Exercise 1
+
+Write a program that asks the user to input a number n, then prints a square of asterisks of size n x n. For example, if the user inputs 4, the program should print:
+
+****
+****
+****
+****
+
+Example input: 5
+Example output:
+*****
+*****
+*****
+*****
+*****
+
+"""
+
+"""
+Exercise 2
+
+Write a program that asks the user to input a number n, then prints a right triangle of asterisks with n rows. For example, if the user inputs 4, the program should print:
+
+*
+**
+***
+****
+
+Example input: 6
+Example output:
+*
+**
+***
+****
+*****
+******
+"""
+
+"""
+Exercise 3
+
+Write a program that asks the user to input two numbers, n and m, then prints a checkerboard pattern of asterisks of size n x m. For example, if the user inputs 4 and 6, the program should print:
+
+* * * * *
+ * * * * *
+* * * * *
+ * * * * *
+
+Example input: 3, 5
+Example output:
+* * * * *
+ * * * * 
+* * * * *
+ * * * * 
+* * * * *
+"""
+
+
+"""
+Exercise 4
+
+Write a program that prints the multiplication table from 1 to 10. The program should print each line in the format "i x j = k", where i and j are the factors being multiplied, and k is their product. 
+For example, the program should print:
+
+1 x 1 = 1
+1 x 2 = 2
+1 x 3 = 3
+...
+10 x 8 = 80
+10 x 9 = 90
+10 x 10 = 100
+
+Example output:
+1 x 1 = 1
+1 x 2 = 2
+1 x 3 = 3
+...
+10 x 9 = 90
+10 x 10 = 100
+"""
