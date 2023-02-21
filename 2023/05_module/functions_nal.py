c1923ff6344268928ff11ed0e2540db4991f0ccf
jakobudovic <ju8168@student.uni-lj.si>
2023-02-21 20:05:57 +0100

    Add functions nal

---
+++ 2023/05_module/functions_nal.py
@@ -0,0 +1,40 @@
+"""
+1. Write a function that takes in two integers and returns their sum.
+"""
+
+
+def add_nums(num1, num2):
+    return num1 + num2
+
+
+"""
+2. Write a function that takes in a list of numbers and returns the average.
+"""
+
+"""
+3. Write a Python function called reverse_string that takes a string as input and returns the reverse of that string. 
+For example, if the input string is "hello", the function should return "olleh".
+Hint: string ni nic drugega kot seznam characterjev.
+"""
+
+"""
+4.
+Write a Python function called is_palindrome that takes a string as input and returns True if the string is a palindrome (i.e., reads the same backward as forward), and False otherwise. 
+For example, if the input string is "racecar", the function should return True.
+"""
+
+"""
+5. Write a function that takes in a string and returns a dictionary with the count of each letter in the string.
+"""
+
+"""
+6. Write a function that takes in a list of integers and returns the largest and smallest numbers in the list.
+def find_min_max([5, 1, 45, 12, -56, 22, 18])
+
+Out:
+print(45, -56)
+"""
+
+"""
+7. Write a function that takes in two lists of integers and returns a new list with the common elements between the two lists.
+"""
