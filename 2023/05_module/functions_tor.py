b76d47bb1477c74df8ad319d9463acac1734bb20
jakobudovic <ju8168@student.uni-lj.si>
2023-02-21 20:07:09 +0100

    Add functions naloge

--- 2023/05_module/functions_tor.py
+++ 2023/05_module/functions_tor.py
@@ -15,7 +15,7 @@ def sestej_stevilia(a, b):
     return vsota
 
 
-odgovor = sestej_stevilia(10, 5)
+odgovor = sestej_stevilia(10, 5)  # 15
 print(odgovor)
 
 
@@ -23,3 +23,33 @@ print(odgovor)
 A parameter is the variable listed inside the parentheses in the function definition.
 An argument is a value that is sent to the function when it is called.
 """
+
+
+def blok_kode(ime, priimek, starost):
+    print(f"Zivjo, {ime}, {starost}")
+    print(f"Danes je 21. feb. 2023")
+    print(f"Ucili se bomo funkcije.")
+
+
+def sprintaj_ime_nkrat(ime, n):
+    for i in range(n):
+        print(f"{ime}")
+
+
+# sprintaj_ime_nkrat("jakob", 5)
+
+
+def razcleni_imena(imena):  # ["ajk", "andraz", "erik", "jan", "luka"]
+    print(f"razclenjujemo imena ...")
+    razclenjena_imena = ", ".join(imena)
+    print(f"narejeno!")
+    return razclenjena_imena
+
+
+imena = ["ajk", "andraz", "erik", "jan", "luka"]
+imena_new = razcleni_imena(imena)
+
+
+print(imena_new)
+
+# print(), len(), max(), min(), ...
