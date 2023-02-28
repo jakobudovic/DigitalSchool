fbf928970d611db28015daf34db515083e08d199
jakobudovic <ju8168@student.uni-lj.si>
2023-02-28 21:57:10 +0100

    Add kviz part 1

---
+++ 2023/05_module/kviz.py
@@ -0,0 +1,15 @@
+"""
+Napisite funkcijo, ki preveri, ali je uporabnikov odgovor na vprasanje pravilen.
+Tekom programa si zapomni stevilo pravilno odgovorjenih vprasanj.
+Na koncu izpisi stevilo nabranih tock.
+
+
+Primer:
+def preveri_odgovor(ugib, odgovor, tocke):
+    # TODO
+    return tocke 
+    
+print("Ugibaj glavna mesta!")
+ugib = input("Ugani glavno mesto Hrvaske: ")
+preveri_odgovor(ugib, "Zagreb", tocke)
+"""
