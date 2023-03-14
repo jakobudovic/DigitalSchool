b048b7a2de854bc1812e710fc8eb94b853c0029e
jakobudovic <ju8168@student.uni-lj.si>
2023-03-14 20:51:25 +0100

    Update kviz instructions

--- 2023/05_module/04_kviz.py
+++ 2023/05_module/04_kviz.py
@@ -1,14 +1,17 @@
 """
+KVIZ
 Napisite funkcijo, ki preveri, ali je uporabnikov odgovor na vprasanje pravilen.
+Napisite program, ki bo vprasal uporabnika vsaj 5 razlicnih vprasanj, on pa mora nanje odgovoriti. 
+Z napisano funkcijo preverite, ali je njegov odgovor pravilen ali ne.
 Tekom programa si zapomni stevilo pravilno odgovorjenih vprasanj.
-Na koncu izpisi stevilo nabranih tock.
+Na koncu izpisi stevilo nabranih tock aka stevilo pravilno odgovorjenih vprasanj.
 
 
 Primer:
 def preveri_odgovor(ugib, odgovor, tocke):
     # TODO
     return tocke 
-    
+
 print("Ugibaj glavna mesta!")
 ugib = input("Ugani glavno mesto Hrvaske: ")
 preveri_odgovor(ugib, "Zagreb", tocke)
