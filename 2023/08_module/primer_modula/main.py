4125490a0ab74ef62731724a2ddb4de3ea4ec5de
jakobudovic <ju8168@student.uni-lj.si>
2023-04-11 20:06:30 +0200

    Add pomozne funkcije module primer

---
+++ 2023/08_module/primer_modula/main.py
@@ -0,0 +1,5 @@
+import pomozne_funkcije
+
+pomozne_funkcije.pozdrav("Uros")
+
+print(pomozne_funkcije.del_abecede[2])
---
+++ 2023/08_module/primer_modula/pomozne_funkcije.py
@@ -0,0 +1,5 @@
+def pozdrav(ime="neznanec"):
+    print(f"Zivjo {ime}!")
+
+
+del_abecede = ["a", "b", "c", "d"]
